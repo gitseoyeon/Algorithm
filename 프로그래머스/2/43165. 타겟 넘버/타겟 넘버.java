8cd045f113607f2e6dfe075b625b1ea1a4b169b2
SeoYeon Min <126096318+gitseoyeon@users.noreply.github.com>
2024-06-03 16:46:51 +0900

    [level 2] Title: 타겟 넘버, Time: 0.52 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,21 @@
+class Solution {
+    public static int answer;
+    public int solution(int[] numbers, int target) {
+        
+        dfs(numbers, 0, 0, target);
+        
+        return answer;
+    }
+    
+    private void dfs(int[] numbers, int idx, int value, int target){
+        if(numbers.length == idx){
+            if(value == target){
+                answer++;
+            }   
+            return;
+        }
+        
+        dfs(numbers, idx + 1, value + numbers[idx], target);
+        dfs(numbers, idx + 1, value - numbers[idx], target);
+    }
+}
