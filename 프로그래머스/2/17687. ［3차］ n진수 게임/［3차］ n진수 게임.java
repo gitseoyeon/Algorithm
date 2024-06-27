c08874aa20ad7112f7d9034893750140658aafa7
SeoYeon Min <126096318+gitseoyeon@users.noreply.github.com>
2024-06-27 17:30:38 +0900

    [level 2] Title: [3차] n진수 게임, Time: 15.42 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,15 +1,24 @@
 class Solution {
     public String solution(int n, int t, int m, int p) {
         String answer = "";
-        String str = "";
         
-        for(int i = 0; i <= t * m; i++){
-            str += Integer.toString(i, n);
+        int num = 0;
+        int cnt = 0;
+        int position = 0;
+
+        while (cnt < t) {
+            String str = Integer.toString(num, n);
+            for (int i = 0; i < str.length(); i++) {
+                if (position % m == p - 1) {
+                    answer += str.charAt(i);
+                    cnt++;
+                    if (cnt == t) break;
+                }
+                position++;
+            }
+            num++;
         }
         
-        for(int i = p - 1; i < t * m; i += m){
-            answer += str.charAt(i);
-        }
         return answer.toUpperCase();
     }
 }
