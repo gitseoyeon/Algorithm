6af8fd63d482c68c8869ab271dd04df078a55507
SeoYeon Min <126096318+gitseoyeon@users.noreply.github.com>
2024-08-05 20:58:28 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 109.09 ms, Memory: 107 MB -BaekjoonHub

---
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] files) {
+        String[] answer = new String[files.length];
+        
+        Arrays.sort(files, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2){
+                String s1 = o1.split("[0-9]")[0];
+                String s2 = o2.split("[0-9]")[0];
+                
+                int sameStr = s1.toLowerCase().compareTo(s2.toLowerCase());
+                if(sameStr == 0) {
+                    int num1 = compareNum(o1.substring(s1.length()));
+                    int num2 = compareNum(o2.substring(s2.length()));
+                    sameStr = num1 - num2;
+                }
+                return sameStr;
+            }
+        });
+        return files;
+    }
+    
+    private int compareNum(String str) {
+        String num = "";
+        
+        for(char c : str.toCharArray()){
+            if(Character.isDigit(c)){
+                num += c;
+            } else {
+                break;
+            }
+        }
+
+        if(num.isEmpty()){
+            return 0;
+        }else{
+            return Integer.parseInt(num);
+        }
+    }
+}
