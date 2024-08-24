cafcc85c4b8430dbf861397e6ae6e88fabed7239
SeoYeon Min <126096318+gitseoyeon@users.noreply.github.com>
2024-08-25 01:48:15 +0900

    [level 2] Title: 가장 큰 수, Time: 2811.05 ms, Memory: 394 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        String[] str = new String[numbers.length];
+    
+        for(int i = 0; i < numbers.length; i++){
+            str[i] = Integer.toString(numbers[i]);
+        }
+        
+        Arrays.sort(str, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        
+        if(str[0].equals("0")){
+            return answer = "0";
+        }else{
+            answer += str[0];
+        }
+        
+        for(int i = 1; i < str.length; i++){
+            answer += str[i];
+        }
+
+        return answer;
+    }
+}
