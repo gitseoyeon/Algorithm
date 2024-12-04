66ea1b5cf9aca032b701bd91d68b38fc969efc33
SeoYeon Min <126096318+gitseoyeon@users.noreply.github.com>
2024-12-04 15:43:06 +0900

    [Silver I] Title: 포도주 시식, Time: 124 ms, Memory: 14900 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] wine = new int[n + 2];
+        int[] dp = new int[n + 2];
+        for(int i = 1; i <= n; i++){
+            wine[i] = Integer.parseInt(br.readLine());
+        }
+        dp[1] = wine[1];
+        dp[2] = wine[1] + wine[2];
+
+        for(int i = 3; i <= n; i++){
+            int tmp = Math.max(dp[i - 1], dp[i - 2] + wine[i]);
+            dp[i] = Math.max(tmp, dp[i - 3] + wine[i] + wine[i - 1]);
+        }
+        System.out.println(dp[n]);
+    }
+}
