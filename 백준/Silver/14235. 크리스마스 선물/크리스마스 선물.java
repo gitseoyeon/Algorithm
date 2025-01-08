98c3bcc7f9e2fa21e2887dc472606633174ae218
SeoYeon Min <126096318+gitseoyeon@users.noreply.github.com>
2025-01-08 12:07:13 +0900

    Update 크리스마스 선물.java

--- 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.java
+++ 백준/Silver/14235. 크리스마스 선물/크리스마스 선물.java
@@ -9,7 +9,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[3];
         PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
 
         for(int i = 0; i < n; i++){
