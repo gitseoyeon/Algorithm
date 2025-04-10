a2c8073355b8a39fd0cab1a9e6e3bcfe0c3171fc
SeoYeon Min <126096318+gitseoyeon@users.noreply.github.com>
2025-04-10 14:09:00 +0900

    [Silver III] Title: 달팽이, Time: 620 ms, Memory: 88224 KB -BaekjoonHub

---
+++ 백준/Silver/1913. 달팽이/달팽이.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int[] dx = {1, 0, -1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[][] arr;
+    static int N, target;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        target = Integer.parseInt(br.readLine());
+        arr = new int[N][N];
+        setArr();
+        
+        StringBuilder sb = new StringBuilder();
+        int x = 0;
+        int y = 0;
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                if(arr[i][j] == target){
+                    x = i + 1;
+                    y = j + 1;
+                }
+                sb.append(arr[i][j] + " ");
+            }
+            sb.append("\n");
+        }
+        sb.append(x + " " + y);
+
+        System.out.println(sb);
+    }
+
+    public static void setArr(){
+        int x = 0;
+        int y = 0;
+        int dir = 0;
+        arr[x][y] = N * N;
+
+        while(dir < 4){
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+
+            if(nx >= 0 && nx < N && ny >= 0 && ny < N && arr[nx][ny] == 0){
+                arr[nx][ny] = arr[x][y] - 1;
+                if(arr[nx][ny] == 1) break;
+                x = nx;
+                y = ny;
+            }else{
+                dir++;
+                if(dir >= 4){
+                    dir = 0;
+                }
+            }
+        }
+    }
+}
