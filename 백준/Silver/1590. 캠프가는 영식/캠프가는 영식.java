53440295e73a79089b17a5867929073d08a10cc6
SeoYeon Min <126096318+gitseoyeon@users.noreply.github.com>
2025-09-23 17:25:44 +0900

    [Silver IV] Title: 캠프가는 영식, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Silver/1590. 캠프가는 영식/캠프가는 영식.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer str = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(str.nextToken());
+        int T = Integer.parseInt(str.nextToken());
+        int time = 1000000;
+        boolean hasBus = false;
+
+        for(int i = 0; i < N; i++) {
+            str = new StringTokenizer(br.readLine());
+            int S = Integer.parseInt(str.nextToken());
+            int I = Integer.parseInt(str.nextToken());
+            int C = Integer.parseInt(str.nextToken());
+
+            int tmp = S;
+            int wait = 1000000;
+            for(int j = 0; j < C; j++) {
+                if(T <= tmp) {
+                    wait = Math.min(tmp - T, wait);
+                    hasBus = true;
+                    break;
+                }
+                tmp += I;
+            }
+            time = Math.min(wait, time);
+        }
+
+        if(hasBus) {
+            System.out.println(time);
+        } else {
+            System.out.println(-1);
+        }
+    }    
+}
